754d0202ec316fec00c27f7d8d1754bced732cc9
22hsantos <22hsantos@wakatipu.school.nz>
2026-04-24 15:09:58 +1200

    feat: setup basic GUI

--- sak_n_pave.py
+++ sak_n_pave.py
@@ -6,3 +6,21 @@ Users can search for specific items from a simple txt file database.
 Users can navigate through the list of stock through various buttons.
 Users can also add, remove, or modify stock information.
 """
+
+from tkinter import *
+from tkinter import messagebox
+
+class MainWindow:
+    """
+    Main application class for navigation and stock display.
+    """
+    def __init__(self,parent):
+        parent.geometry("400x400")
+
+        testlbl = Label(parent, text="This is the main window.")
+        testlbl.pack()
+
+if __name__ == "__main__":
+    root = Tk()
+    main = MainWindow(root)
+    root.mainloop()
