8b7ca7948be3bd54593fdaec4a03cc25a355cd04
22hsantos <22hsantos@wakatipu.school.nz>
2026-04-24 14:51:18 +1200

    Added the explanatory docstring

---
+++ sak_n_pave.py
@@ -0,0 +1,8 @@
+"""
+SaknPave: Graphical inventory system
+
+This script uses a GUI to present stock in a readable format.
+Users can search for specific items from a simple txt file database.
+Users can navigate through the list of stock through various buttons.
+Users can also add, remove, or modify stock information.
+"""
