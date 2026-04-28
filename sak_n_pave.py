dba03d647a6131dcbc6ad0908f201bba3305f03b
22hsantos <22hsantos@wakatipu.school.nz>
2026-04-29 10:03:12 +1200

    style: changed docstrings into comments

--- sak_n_pave.py
+++ sak_n_pave.py
@@ -15,21 +15,22 @@ class MainWindow:
     Main application class for navigation and stock display.
     """
     def __init__(self,parent):
+        #layout configuration
         parent.geometry("400x400")
-
-        """layout configuration"""
         parent.grid_rowconfigure(0, weight=1)
         parent.grid_columnconfigure(0, weight=1)
 
-        """title frame"""
+        #title frame
         self.title_frame = Frame(parent,bg= "lightyellow")
         self.title_frame.grid(column=0,row=0, sticky= NSEW)
         self.title_frame.grid_rowconfigure(0, weight=1)
         self.title_frame.grid_columnconfigure(0, weight=1)
 
-        """main menu title"""
+        #navigation frame
+
+        #main menu title
         menu_title = Label(
-            parent,
+            self.title_frame,
             text= "Welcome to Sak n Pave Inventory system.",
             background= "lightyellow"
         )
